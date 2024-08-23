b7043dc3467287163c21f39f0f47efba49a19c36
ShivanshKumarJha <kumarjhashivansh@gmail.com>
2024-08-23 23:30:23 +0530

    Initialised the project

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+
+const { PORT, MONGODB_URI } = require('./config/environments');
+
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+// const multer = require('multer');
+const mongoose = require('mongoose');
+
+const app = express();
+const cors = require('cors');
+
+// const routers1 = require('vfbvFB');
+// const routers2 = require('vfbvFB');
+
+app.use(express.urlencoded());
+app.use(cookieParser());
+app.use(cors());
+
+app.use('/', (req, res) => {
+  res.send({ message: 'Hello' });
+});
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  next();
+});
+
+// app.use('/feqwed', routers1);
+// app.use('/ffff', routerr2);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(result => {
+    console.log(`App is listening on the port:${PORT}`);
+    app.listen(PORT);
+    console.log('Database connected..');
+  })
+  .catch(err => console.log(err));
